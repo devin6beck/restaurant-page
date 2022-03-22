6f8e49dfa3585f0f543a2b4abe178881269d1a5a
Devin Beck <devin6beck@gmail.com>
2022-03-21 18:06:27 -0600

    refactor home.js

--- src/home.js
+++ src/home.js
@@ -1,7 +1,33 @@
 const Homepage = (function() {
-  function generate() {
+  function createImagesDiv() {
+    const imagesDiv = document.createElement('div');
+    imagesDiv.classList.add('food-images');
+  
+    const image1 = document.createElement('img');
+    image1.src = 'images/gourmet-food-spread.jpg';
+    image1.alt = 'gourmet food spread';
+  
+    const image2 = document.createElement('img');
+    image2.src = 'images/gourmet-wine-spread-jonathan-borba.jpg';
+    image2.alt = 'gourmet food spread with wine';
+  
+    const image3 = document.createElement('img');
+    image3.src = 'images/steak-justus-menke.jpg';
+    image3.alt = 'gourmet steak';
+  
+    const image4 = document.createElement('img');
+    image4.src = 'images/dessert- anna-tukhfatullina.jpg';
+    image4.alt = 'gourmet dessert';
+  
+    imagesDiv.appendChild(image1);
+    imagesDiv.appendChild(image2);
+    imagesDiv.appendChild(image3);
+    imagesDiv.appendChild(image4);
+    return imagesDiv
+  }
+
+  function createMain() {
     const main = document.createElement('main');
-    main.classList.add('current-page');
 
     const h2 = document.createElement('h2');
     h2.textContent = 'Description';
@@ -12,34 +38,23 @@ const Homepage = (function() {
     const p2 = document.createElement('p');
     p2.textContent = 'The guides handle the labor and our gourmet chef prepares bomb food.';
 
-    const imagesDiv = document.createElement('div');
-    imagesDiv.classList.add('food-images');
-
-    const image1 = document.createElement('img');
-    image1.src = 'images/gourmet-food-spread.jpg';
-    image1.alt = 'gourmet food spread';
-
-    const image2 = document.createElement('img');
-    image2.src = 'images/gourmet-wine-spread-jonathan-borba.jpg';
-    image2.alt = 'gourmet food spread with wine';
-
-    const image3 = document.createElement('img');
-    image3.src = 'images/steak-justus-menke.jpg';
-    image3.alt = 'gourmet steak';
-
-    const image4 = document.createElement('img');
-    image4.src = 'images/dessert- anna-tukhfatullina.jpg';
-    image4.alt = 'gourmet dessert';
-
-    imagesDiv.appendChild(image1);
-    imagesDiv.appendChild(image2);
-    imagesDiv.appendChild(image3);
-    imagesDiv.appendChild(image4);
     main.appendChild(h2);
     main.appendChild(p1);
     main.appendChild(p2);
-    main.appendChild(imagesDiv);
     return main;
+  }
+
+  function generate() {
+    const homePage = document.createElement('div');
+    homePage.classList.add('current-page');
+    const main = createMain();
+    const imagesDiv = createImagesDiv();
+
+    homePage.appendChild(imagesDiv);
+    homePage.appendChild(main);
+
+    return homePage;
+
   }
   return {generate};
 })();
--- src/initial.js
+++ src/initial.js
@@ -2,42 +2,87 @@ import Home from "./home";
 
 function createHeader() {
   
-
   const header = document.createElement('header');
   header.classList.add('header');
 
   const title = document.createElement('h1');
+  title.classList.add('h1');
   title.textContent = 'Chama Canteen';
 
-  header.appendChild(title);
+  const logoContainer = document.createElement('div');
+  logoContainer.classList.add('logo-container');
 
+  const logo = document.createElement('img');
+  logo.src = './images/zia-red.png';
+  logo.alt = 'zia symbol';
+  logo.classList.add('logo');
+  logoContainer.appendChild(logo);
+  
   const nav = document.createElement('nav');
-  const tabs = document.createElement('ul');
-  tabs.classList.add('tabs');
-  const homeTab = document.createElement('li');
+  nav.classList.add('tabs');
+
+  const homeTab = document.createElement('a');
+  homeTab.textContent = 'Home';
   homeTab.classList.add('home');
   homeTab.classList.add('tab');
-  homeTab.value = "home";
-  homeTab.textContent = 'Home';
-  const menuTab = document.createElement('li');
+
+  const menuTab = document.createElement('a');
+  menuTab.textContent = 'Menu';
   menuTab.classList.add('menu');
   menuTab.classList.add('tab');
-  menuTab.textContent = 'Menu';
-  const contactTab = document.createElement('li');
+
+  const contactTab = document.createElement('a');
+  contactTab.textContent = 'Contact';
   contactTab.classList.add('contact');
   contactTab.classList.add('tab');
-  contactTab.textContent = 'Contact';
-  tabs.appendChild(homeTab);
-  tabs.appendChild(menuTab);
-  tabs.appendChild(contactTab);
 
-  nav.appendChild(tabs);
+  nav.appendChild(homeTab);
+  nav.appendChild(menuTab);
+  nav.appendChild(contactTab);
 
+
+  header.appendChild(title);
+  header.appendChild(logoContainer);
   header.appendChild(nav);
-  
 
   return header;
 
+}
+
+function createSpacer() {
+  const spacer = document.createElement('div');
+  spacer.classList.add('spacer');
+
+  return spacer;
+}
+
+function createImageContainer() {
+  const imageContainer = document.createElement('div');
+  imageContainer.classList.add('food-images');
+  imageContainer.classList.add('current-page');
+
+  const foodSpreadImage = document.createElement('img');
+  foodSpreadImage.src = './images/gourmet-food-spread.jpg';
+  foodSpreadImage.alt = 'gourmet food spread';
+
+  const wineSpreadImage = document.createElement('img');
+  foodSpreadImage.src = './images/gourmet-wine-spread-jonathan-borba.jpg';
+  foodSpreadImage.alt = 'gourmet food spread';
+
+  const steakImage = document.createElement('img');
+  foodSpreadImage.src = './images/steak-justus-menke.jpg';
+  foodSpreadImage.alt = 'gourmet steak';
+
+  const dessertImage = document.createElement('img');
+  foodSpreadImage.src = './images/dessert- anna-tukhfatullina.jpg';
+  foodSpreadImage.alt = 'gourmet dessert';
+
+  imageContainer.appendChild(foodSpreadImage);
+  imageContainer.appendChild(wineSpreadImage);
+  imageContainer.appendChild(steakImage);
+  imageContainer.appendChild(dessertImage);
+
+  return imageContainer;
 }
 
 function createFooter() {
@@ -56,28 +101,19 @@ function createFooter() {
 
   return footer;
 }
-/* 
-function createRight() {
-  const aside = document.createElement('aside');
-  aside.classList.add('right');
-  const image = document.createElement('img');
-  image.src = 'images/rafts-chama.jpg';
-  aside.appendChild(image);
 
-  return aside;
-}
- */
 function initialPage() {
-  const content = document.querySelector('.content')
+  const body = document.querySelector('body')
   const header = createHeader();
   const homepage = Home.generate();
   const footer = createFooter();
-  // const right = createRight();
 
-  content.appendChild(header);
-  content.appendChild(homepage);
-  content.appendChild(footer);
-  // content.appendChild(right);
+  body.appendChild(header);
+  body.appendChild(createSpacer());
+  body.appendChild(homepage);
+  body.appendChild(createSpacer());
+  body.appendChild(footer);
+
 
 
 }
